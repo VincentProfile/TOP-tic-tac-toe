949f47607cd455d712fd124785cd5084f0cd4942
Vincent <vincentccs93@gmail.com>
2021-03-22 16:51:32 +0800

    add function to get player name

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const gameboard = (()=>{
         button.innerText = 'X';
     })
     // return methods
-    return {fillBoard, score, scoreBoard};
+    return {fillBoard};
 })();
+
 const scoreBoard = (()=> {
     const score = () => {
 
@@ -18,8 +19,75 @@ const scoreBoard = (()=> {
     return {score, overallScore};
 })();
 
+const playerNames = [];
 const displayController = (() => {
-    
+
+    const choosePlayerTypeDiv = document.querySelector('.choosePlayerTypeDiv');
+    const instruction = document.querySelector('.instruction');
+    const startBtn = document.querySelector('.startBtn');
+    const playerBtn = document.querySelector('.playerBtn');
+    const playerNameDiv = document.querySelector('.playerNameDiv');
+    const playerNameBtn = document.querySelector('.playerNameBtn');
+    const computerBtn = document.querySelector('.computerBtn');
+    const playerNameInput = document.querySelector('.playerName');
+    const gameBoard = document.querySelector('.gameBoard');
+    const resetBtn = document.querySelector('.resetBtn');
+
+    const showPlayerSelection = (sequence) => {
+        if (sequence != 'Second')
+            sequence = 'First';
+        startBtn.style.display = 'none';
+
+        choosePlayerTypeDiv.style.display = 'flex';
+
+        instruction.innerText = `Select ${sequence} Contender`;
+        instruction.style.display = 'flex';
+    }
+    startBtn.addEventListener('click', showPlayerSelection);
+
+    const showPlayerNameDiv = () => {
+
+        playerNameDiv.style.display = 'unset';
+        choosePlayerTypeDiv.style.display = 'none';
+        playerNameInput.value = '';
+    }
+    playerBtn.addEventListener('click', showPlayerNameDiv);
+
+    const getPlayerName = () => {
+        if (playerNameInput.value != ""){
+            if (playerNames.length < 1){
+                playerNames.push(playerNameInput.value);
+                showPlayerSelection('Second');
+            }
+            else{
+                playerNames.push(playerNameInput.value);
+                instruction.style.display = 'none';
+                gameBoard.style.display = 'block';
+                resetBtn.style.display = 'unset';
+            }
+            playerNameDiv.style.display = 'none';
+        }
+    }
+    playerNameBtn.addEventListener('click', getPlayerName);
+
+    const getComputerName = () => {
+        if (playerNames.length < 1){
+            playerNames.push('ComputerOne');
+            showPlayerSelection('Second');
+            computerBtn.style.display = 'none';
+        }
+        else{
+            playerNames.push('ComputerTwo');
+            choosePlayerTypeDiv.style.display = 'none';
+            instruction.style.display = 'none';
+            gameBoard.style.display = 'block';
+            resetBtn.style.display = 'unset';
+        }
+    }
+    computerBtn.addEventListener('click', getComputerName);
+
+    // return methods 
+    return {};
 })();
 
 // player
